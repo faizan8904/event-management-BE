d3e8b60a5477a55bda334cf363cab1740976b662
Faizan <faizansyed8904@gmail.com>
2025-03-27 23:47:00 +0530

    module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, EventsModule, TicketsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
